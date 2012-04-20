e33ef1fe69020fb08487c2984c87f753d065eafb
issm <issmxx@gmail.com>
2012-04-21 00:44:49 +0900

    initial commit

---
+++ lib/Text/Markdown/Pluggable.pm
@@ -0,0 +1,32 @@
+package Text::Markdown::Pluggable;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Text::Markdown::Pluggable -
+
+=head1 SYNOPSIS
+
+  use Text::Markdown::Pluggable;
+
+=head1 DESCRIPTION
+
+Text::Markdown::Pluggable is
+
+=head1 AUTHOR
+
+issm E<lt>issmxx@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
